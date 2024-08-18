afec8f852dfba0232a20d6bb7b972c495409a508
badis <benalibadis@gmail.com>
2024-08-18 23:51:25 +0200

    feat:postgres

--- metaflow/src/config/connector.rs
+++ metaflow/src/config/connector.rs
@@ -3,12 +3,20 @@ use std::sync::Arc;
 use connector::DataConnector;
 use connector::http::HttpClient;
 use connector::influxdb::InfluxDbClient;
+use connector::postgresql::PostgresClient;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum ConnectorConfig {
     HttpClient,
     InfluxDbClient { url: String, token: String },
+    PostgresClient {
+        host: String,
+        port: u16,
+        user: String,
+        password: String,
+        dbname: String,
+    },
 }
 
 impl ConnectorConfig {
@@ -16,6 +24,12 @@ impl ConnectorConfig {
         match self {
             ConnectorConfig::HttpClient => Arc::new(HttpClient::new()),
             ConnectorConfig::InfluxDbClient { url, token } => Arc::new(InfluxDbClient::new(url, token)),
+            ConnectorConfig::PostgresClient { host, port, user, password, dbname } => { // TODO : to update
+                let client = tokio::runtime::Handle::current().block_on(PostgresClient::new(
+                    &format!("host={} port={} user={} password={} dbname={}", host, port, user, password, dbname)
+                )).unwrap();
+                Arc::new(client)
+            },
         }
     }
 }
